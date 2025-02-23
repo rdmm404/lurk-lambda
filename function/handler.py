b7e627f0764d022b12037b50152c6ccb71bb6fd4
rdmm123 <robedanilo2001@gmail.com>
2025-02-22 22:25:31 -0500

    increase memory size to avoid timeout

--- function/handler.py
+++ function/handler.py
@@ -2,18 +2,21 @@ import os
 
 import subprocess
 import sys
-import boto3
 
 
 def get_telegram_credentials() -> tuple[str, str]:
+    import boto3
     token = os.getenv("LURK_TELEGRAM_TOKEN")
     chat_id = os.getenv("LURK_TELEGRAM_CHAT_ID")
 
     ssm_client = boto3.client("ssm")
 
     try:
-        token = ssm_client.get_parameter(Name=token, WithDecryption=True)
-        chat_id = ssm_client.get_parameter(Name=chat_id, WithDecryption=True)
+        token_response = ssm_client.get_parameter(Name=token, WithDecryption=True)
+        chat_id_response = ssm_client.get_parameter(Name=chat_id, WithDecryption=True)
+
+        token = token_response.get("Parameter", {}).get("Value", "")
+        chat_id = chat_id_response.get("Parameter", {}).get("Value", "")
     except Exception as e:
         print(
             f"Something happened while getting the telegram credentials: {e}. Using env vars instead"
@@ -25,11 +28,10 @@ def get_telegram_credentials() -> tuple[str, str]:
 def handler(event, context):
     try:
         token, chat_id = get_telegram_credentials()
-        print(token, chat_id)
         subprocess.run(
             [sys.executable, "-m", "lurk", "run"],
             check=True,
-            env={"LURK_TELEGRAM_TOKEN": token, "LURK_TELEGRAM_CHAT_ID": chat_id},
+            env=dict(os.environ, LURK_TELEGRAM_TOKEN=token, LURK_TELEGRAM_CHAT_ID=chat_id),
         )
         return {"statusCode": 200, "body": "Evertything went well"}
     except subprocess.CalledProcessError as e:
